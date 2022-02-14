ea317da56b0abce0fa9a7d02b59aa22e6f069ffa
NathanHale <nicholasnavarro98@gmail.com>
2022-02-14 19:44:38 +0200

    moved docker files to .dockerignore directory, changes to config to use pgPool

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 class Config:
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://admin:password@postgres:5432/WorkoutsDB'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://admin:password@pgpool/WorkoutsDB'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = 'super-secret-key'
